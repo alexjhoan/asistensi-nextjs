251950907554ef91337afec5ccd3dd2dffc0f2fb
alexjhoan <alexjhoan.25@gmail.com>
2021-11-16 14:39:58 -0400

    fix menor bug

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,7 +24,7 @@ export default function Home() {
           <div className="sm:mt-20 2xl:mt-32">
             <h1 className="text-3xl lg:text-5xl text-white font-extrabold leading-tight mb-6 lg:mb-12 mt-0">Special Way <br/>To Realize Your Own <br/>Bussines.</h1>
             <p className="text-xl text-white ">Are you going to make ypur website? Share your idea with us or make it by yourself with our project maker software. Make it happens today!</p>
-            <div className="flex flex-col lg:flex-row mt-6 lg:mt-14 space-y-4 lg:space-x-8 lg:space-y-0">
+            <div className="flex flex-col xl:flex-row mt-6 xl:mt-14 space-y-4 xl:space-x-8 xl:space-y-0">
               <button className="bg-blue-400 border-2 border-blue-400 border-opacity-100  text-white px-8 py-3 rounded-3xl transition-colors duration-500 ease-in-out hover:bg-transparent">Get it started</button>
               <button className="bg-transparent border-2 border-white border-opacity-100 text-white px-8 py-3 rounded-3xl transition-colors duration-500 ease-in-out hover:bg-white hover:text-purple-700 ">Get it started</button>
             </div>
