5b80b25d1dca5b59bafe3aef1758ff2332ab3d4a
alexjhoan <alexjhoan.25@gmail.com>
2021-11-16 15:32:38 -0400

    fix menor bug

--- components/Users.jsx
+++ components/Users.jsx
@@ -10,7 +10,7 @@ const Users = ({users}) => {
     <div className='container items-center justify-center mx-auto my-4 mb-96 md:mb-0'>
       {
         users.map(u => (
-          <div key={u.id} className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 justify-center'>
+          <div key={u.id} className='grid grid-cols-1 md:grid-cols-2 gap-1 mb-5 justify-center'>
             <h1>{u.name}</h1>
             <button className="bg-gray-200 bg-opacity-25 border-2 border-white border-opacity-100 text-white px-4 py-2 rounded-3xl transition-colors duration-500 ease-in-out hover:bg-white hover:text-purple-700 w-60 mx-auto" onClick={()=>dispatch({type:'REMOVE_USER',id:u.id})}>eliminar</button>
           </div>
